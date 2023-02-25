bbae829f3b25b19e172d9da670956bcecaabd1a0
vishnuedclub <vishnuvardhan@edclub.in>
2023-02-25 14:14:09 +0530

    major code bugs resolved

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import * as $ from 'jquery';
 import { trigger, state, style, transition, animate } from '@angular/animations';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -14,7 +15,7 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
       state('out', style({
         transform: 'translateY(-100%)'
       })),
-      transition('in => out', animate('0.1s ease-out')),
+      transition('in => out', animate('0.3s ease-out')),
       transition('out => in', animate('0.3s ease-in'))
     ])
   ]
@@ -32,15 +33,33 @@ export class MenuComponent implements OnInit {
   public toggleRightOffcanvas() {
     this.isRightMenuOpen = !this.isRightMenuOpen;
   }
-  
+  public closeLeftOffCanvas(): void{
+    this.isLeftMenuOpen = false;
+  }
+  public closeRightOffCanvas(): void{
+    this.isRightMenuOpen = false;
+  }
   
   toggleNavbar(){
     this.navbarOpen = !this.navbarOpen;
   }
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  get headerClass(): string {
+    // get current route
+    const currentRoute = this.router.url;
+
+    // check if current route is home page
+    if (currentRoute === '/') {
+      return 'header-white';
+    } else {
+      return 'header-grey';
+    }
+  }
 
   ngOnInit(): void {
   }
+  
 
 }
